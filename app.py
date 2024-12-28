56824a90a25f9f94e427b2e9dcee367a9dbd09d8
LAPTOP-HM059CAI\recep <recep.ua@hotmail.com>
2024-12-28 20:25:57 +0300

    test commit

--- app.py
+++ app.py
@@ -1,25 +1,57 @@
 from flask import Flask, render_template, request, redirect, url_for
-import json
+import sqlite3
 from datetime import datetime
+import os
+from contextlib import contextmanager
 
 app = Flask(__name__)
 
-def load_blogs():
+@contextmanager
+def get_db():
+    conn = sqlite3.connect('blogs.db')
     try:
-        with open('blogs.json', 'r', encoding='utf-8') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
+        yield conn
+    finally:
+        conn.close()
 
-def save_blogs(blogs):
-    with open('blogs.json', 'w', encoding='utf-8') as file:
-        json.dump(blogs, file, ensure_ascii=False, indent=4)
+def init_db():
+    conn = sqlite3.connect('blogs.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS blogs
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  baslik TEXT NOT NULL,
+                  icerik TEXT NOT NULL,
+                  tarih TEXT NOT NULL)''')
+    conn.commit()
+    conn.close()
 
-def format_blog_content():
-    blogs = load_blogs()
-    for blog in blogs:
-        blog['icerik'] = blog['icerik'].replace('\n', '<br>')
-    save_blogs(blogs)
+def load_blogs():
+    with get_db() as conn:
+        c = conn.cursor()
+        c.execute('SELECT * FROM blogs')
+        blogs = [{'id': row[0], 'baslik': row[1], 'icerik': row[2], 'tarih': row[3]} 
+                for row in c.fetchall()]
+        return blogs
+
+def save_blog(baslik, icerik):
+    with get_db() as conn:
+        c = conn.cursor()
+        c.execute('INSERT INTO blogs (baslik, icerik, tarih) VALUES (?, ?, ?)',
+                (baslik, icerik, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        conn.commit()
+
+def update_blog(id, baslik, icerik):
+    with get_db() as conn:
+        c = conn.cursor()
+        c.execute('UPDATE blogs SET baslik=?, icerik=?, tarih=? WHERE id=?',
+                (baslik, icerik, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), id))
+        conn.commit()
+
+def delete_blog_db(id):
+    with get_db() as conn:
+        c = conn.cursor()
+        c.execute('DELETE FROM blogs WHERE id=?', (id,))
+        conn.commit()
 
 @app.route('/')
 def home():
@@ -29,52 +61,48 @@ def home():
 
 @app.route('/blog/<int:id>')
 def blog_detail(id):
-    blogs = load_blogs()
-    blog = next((blog for blog in blogs if blog['id'] == id), None)
-    if blog:
+    conn = sqlite3.connect('blogs.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM blogs WHERE id=?', (id,))
+    row = c.fetchone()
+    conn.close()
+    
+    if row:
+        blog = {'id': row[0], 'baslik': row[1], 'icerik': row[2], 'tarih': row[3]}
         return render_template('blog_detail.html', blog=blog)
     return redirect(url_for('home'))
 
 @app.route('/yeni-blog', methods=['GET', 'POST'])
 def new_blog():
     if request.method == 'POST':
-        blogs = load_blogs()
-        new_id = len(blogs) + 1
-        new_blog = {
-            'id': new_id,
-            'baslik': request.form['baslik'],
-            'icerik': request.form['icerik'],
-            'tarih': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        }
-        blogs.append(new_blog)
-        save_blogs(blogs)
+        save_blog(request.form['baslik'], request.form['icerik'])
         return redirect(url_for('home'))
     return render_template('new_blog.html')
 
 @app.route('/blog/sil/<int:id>')
-def delete_blog(id):
-    blogs = load_blogs()
-    blogs = [blog for blog in blogs if blog['id'] != id]
-    save_blogs(blogs)
+def delete_blog_route(id):
+    delete_blog_db(id)
     return redirect(url_for('home'))
 
 @app.route('/blog/duzenle/<int:id>', methods=['GET', 'POST'])
 def edit_blog(id):
-    blogs = load_blogs()
-    blog = next((blog for blog in blogs if blog['id'] == id), None)
+    conn = sqlite3.connect('blogs.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM blogs WHERE id=?', (id,))
+    row = c.fetchone()
+    conn.close()
     
-    if not blog:
+    if not row:
         return redirect(url_for('home'))
     
+    blog = {'id': row[0], 'baslik': row[1], 'icerik': row[2], 'tarih': row[3]}
+    
     if request.method == 'POST':
-        blog['baslik'] = request.form['baslik']
-        blog['icerik'] = request.form['icerik']
-        blog['tarih'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        save_blogs(blogs)
+        update_blog(id, request.form['baslik'], request.form['icerik'])
         return redirect(url_for('blog_detail', id=id))
     
     return render_template('edit_blog.html', blog=blog)
 
 if __name__ == '__main__':
-    format_blog_content()
-    app.run(debug=True) 
+    init_db()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
